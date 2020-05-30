120b0e87e67b3d572ada1eda9db5e9e253594101
cltty <claudiu.timofte@protonmail.com>
2020-05-30 21:31:19 +0300

    added app skeleton

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Book Review</title>
+</head>
+<body>
+    <main class="container">
+        <div class="container__title-box">
+            <h1 class="container__title-box--title">Book review</h1>
+        </div>
+        <div class="container__button-box">
+            <a class="container__button-box--button" href="login.php">Login</a>
+            <a class="container__button-box--button" href="signin.php">Sign in</a>
+            <a class="container__button-box--button" href="about.php">About</a>
+            <a class="container__button-box--button" href="contact.php">Contact</a>
+            <a class="container__button-box--button" href="support.php">Support</a>
+        </div>
+        <div class="container__dynamic-box"></div>
+    </main>
+</body>
+</html>
