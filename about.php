0e2e1f0f6cb4fed65293ff5d129911c48b100511
cltty <claudiu.timofte@protonmail.com>
2020-06-11 02:32:21 +0300

    minor fixs

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/about.css">
+    <title>About</title>
+</head>
+<body>
+    <section class="container">
+        <div class="container__title">
+            <h2>Who We Are</h2>
+        </div>
+        <div class="container__body">BoRe is a site for readers and book recommendations. Our mission is to help people find and share books they love.</div>
+    </section>
+</body>
+</html>
---
+++ support.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/about.css">
+    <title>Support</title>
+</head>
+<body>
+    <section class="container">
+        <div class="container__title">
+            <h2>Support</h2>
+        </div>
+        <div class="container__body">You can support by following us on social media <a href="#">Click here</a></div>
+    </section>
+</body>
+</html>
